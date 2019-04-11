25069bb58c6c2666b3566ff742d78586b9a9404e
Guilherme <guilhermebritogasparini@gmail.com>
2019-04-11 12:01:27 -0300

    Seller details and eager loading

--- SalesWebMvc/Services/SellersService.cs
+++ SalesWebMvc/Services/SellersService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace SalesWebMvc.Services
 {
@@ -16,6 +17,7 @@ namespace SalesWebMvc.Services
 
         public List<Seller> FindAll()
         {
+
             return _context.Seller.ToList();
         }
 
@@ -27,7 +29,7 @@ namespace SalesWebMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
